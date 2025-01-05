9382fed5822059b45fc6b4b025a47e63dd6846db
Rafał Kuczmiński <Locer93@gmail.com>
2025-01-05 19:02:39 +0100

    Added Users pages

---
+++ Pages/Users/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using InventoryApp.Data;
+using InventoryApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
+namespace InventoryApp.Pages.Users
+{
+	[Authorize(Policy = "RequireAdministratorRole")]
+	public class CreateModel : PageModel
+	{
+		private readonly InventoryApp.Data.ApplicationDbContext _context;
+
+		public CreateModel(InventoryApp.Data.ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[BindProperty]
+		public IdentityUser User { get; set; } = default!;
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			_context.Users.Add(User);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
---
+++ Pages/Users/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using InventoryApp.Data;
+using InventoryApp.Models;
+using System.Security.Claims;
+
+namespace InventoryApp.Pages.Users
+{
+	[Authorize(Policy = "RequireAdministratorRole")]
+	public class IndexModel : PageModel
+	{
+		private readonly InventoryApp.Data.ApplicationDbContext _context;
+
+		public IndexModel(InventoryApp.Data.ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public IList<IdentityUser> Users { get; set; } = default!;
+
+		public async Task OnGetAsync()
+		{
+			Users = await _context.Users.ToListAsync();
+		}
+	}
+}
